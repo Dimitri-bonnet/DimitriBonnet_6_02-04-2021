f987aa0cb8ab2b2dacd14d49816b0b2f170b9b6e
= <dimitri@zerocodeautomation.com>
2021-04-16 17:25:37 +0200

    add script modal.js, create item photographer and clean main.js

--- js/modal.js
+++ js/modal.js
@@ -1,4 +1,28 @@
 /* MODAL */
-/* Open modal */
+/* Dom elements */
+const modal = document.querySelector(".bgGround");
+const clossModal = document.querySelectorAll(".modal__close");
+const contentModalForm = document.querySelector(".modal__content-form");
+const contentModalPicture = document.querySelector(".modal__content-picture");
+const openModalPicutre = document.querySelectorAll(".modalOpenPicture");
+const openModalForm = document.querySelectorAll(".modalOpenForm");
+/* OpenModal */
+const lauchModal = (e) => {
+  modal.style.display = "block";
+  if (e.target.classList.contains("modalOpenForm")) {
+    contentModalPicture.style.display = "none";
+    contentModalForm.style.display = "block";
+    console.log("openModalForm");
+  } else if (e.target.classList.contains("modalOpenPicture")) {
+    contentModalForm.style.display = "none";
+    contentModalPicture.style.display = " block";
+    console.log("openModalPicture");
+  }
+};
+openModalPicutre.forEach((btn) => btn.addEventListener("click", lauchModal));
+openModalForm.forEach((btn) => btn.addEventListener("click", lauchModal));
 /* Close modal */
-console.log('modal')
+const closeModal = () => {
+  modal.style.display = "none";
+};
+clossModal.forEach((btn) => btn.addEventListener("click", closeModal));
